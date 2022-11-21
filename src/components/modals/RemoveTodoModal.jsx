1880db8b0fa779a6b266cb05a54e605ffc972793
khloptsev <p.khloptsev@gmail.com>
2022-11-21 22:57:19 +1000

    Создал мод.окно Удалить Todo

---
+++ src/components/modals/RemoveTodoModal.jsx
@@ -0,0 +1,49 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import { removeTodo } from '../../redux/slices/todoSlice';
+import { closeModal } from '../../redux/slices/modalSlice';
+
+const RemoveTodoModal = () => {
+  const dispatch = useDispatch();
+  const selector = useSelector((state) => state.modalState);
+
+  const { id } = selector.extra;
+
+  const [isDisabled, setIsDisabled] = useState(false);
+  const handleRemoveButton = () => {
+    dispatch(removeTodo(id));
+    setIsDisabled(!isDisabled);
+  };
+  const handleChancelButton = () => dispatch(closeModal());
+  return (
+    <>
+      <Modal.Header closeButton>
+        <Modal.Title>Удалить?</Modal.Title>
+      </Modal.Header>
+      <Modal.Footer>
+        <Button
+          onClick={handleRemoveButton}
+          type="button"
+          className="btn btn-danger me-2"
+          disabled={isDisabled}
+        >
+          Удалить
+        </Button>
+        <Button
+          onClick={handleChancelButton}
+          type="button"
+          className="btn btn-secondary"
+          disabled={isDisabled}
+        >
+          Отменть
+        </Button>
+      </Modal.Footer>
+    </>
+  );
+};
+
+export default RemoveTodoModal;
