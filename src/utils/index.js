9d8db040f7d1e3add6c5d000b2fe511a6f4629c2
khloptsev <p.khloptsev@gmail.com>
2022-11-21 10:44:19 +1000

    добавлена функция нормализации и сортировки todo

---
+++ src/utils/index.js
@@ -0,0 +1,18 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable no-unused-vars */
+import dayjs from 'dayjs';
+
+export default (snapshot) => {
+  const todoList = snapshot.docs.map((doc) => {
+    const newDueDate = dayjs(doc.data().dueDate).format('YYYY-MM-DD');
+    const todo = { id: doc.id, ...doc.data(), dueDate: newDueDate };
+    return todo;
+  });
+  return todoList.sort((a, b) => {
+    const aCreated = dayjs(a.createdAt).format('YYYY-MM-DD HH:mm:ss');
+    const bCreated = dayjs(b.createdAt).format('YYYY-MM-DD HH:mm:ss');
+    const aDate = new Date(aCreated);
+    const bDate = new Date(bCreated);
+    return bDate - aDate;
+  });
+};
