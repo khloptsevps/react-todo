ca8e2f82a038241105bbdd3b24939af18e7291c1
khloptsev <p.khloptsev@gmail.com>
2022-11-21 23:20:44 +1000

    Декомпозировал на компоненты

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import { fetchAllTodos } from './redux/slices/todoSlice.js';
 
 import styles from './less/main.less';
 
 import Header from './components/header';
+import AddTodoButton from './components/AddTodoButton/index.jsx';
 import TodoList from './components/todoList';
+import MyModal from './components/modals/Modal.jsx';
 
-const App = () => (
-  <div className={styles.wrapper}>
-    <Header />
-    <div className={styles.container}>
-      <div className={styles.add}>
-        <button type="button" className="btn btn-primary">
-          Add TODO
-        </button>
+const App = () => {
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    dispatch(fetchAllTodos());
+  });
+
+  return (
+    <div className={styles.wrapper}>
+      <Header />
+      <div className={styles.container}>
+        <div className={styles.add}>
+          <AddTodoButton />
+        </div>
+        <div className={styles.todos}>
+          <TodoList />
+        </div>
       </div>
-      <div className={styles.todos}>
-        <TodoList />
-      </div>
+      <MyModal />
     </div>
-  </div>
-);
+  );
+};
 
 export default App;
