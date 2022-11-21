a16701ddfc0fd9cb97233c428b0410cb9356c3a4
khloptsev <p.khloptsev@gmail.com>
2022-11-21 22:49:43 +1000

    добавлен Redux в Header, информация по todo

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import styles from './Header.module.less';
 
-const Header = () => (
-  <header className={styles.root}>
-    <h1 className={styles.title}>Todo</h1>
-    <div className="count">
-      <span className="text">
-        Active Todo:
-        <span className={styles.counter}> 3</span>
-      </span>
-    </div>
-  </header>
-);
+const Header = () => {
+  const selector = useSelector((state) => state.todosState);
+  const { todos } = selector;
+  const notCompletedTodos = todos.filter((todo) => !todo.complete);
+  const completedTodos = todos.filter((todo) => todo.complete);
+  return (
+    <header className={styles.root}>
+      <h1 className={styles.title}>Todo</h1>
+      <div className="count">
+        <div className="text">
+          Всего задач:
+          <span className={styles.counter}> {todos.length}</span>
+        </div>
+        <div className="text">
+          Активные:
+          <span className={styles.counter}> {notCompletedTodos.length}</span>
+        </div>
+        <div className="text text-success">
+          Завершенные:
+          <span className={styles.counter}> {completedTodos.length}</span>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
